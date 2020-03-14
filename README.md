2477a874f128e9ed2151fd93edf11077c3b203ae
Romans Potasovs <justgook@gmail.com>
2020-03-15 01:08:42 +0200

    Fix build

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-https://forums.rpgmakerweb.com/index.php?threads/mv-added-resource-generators-a-face-for-your-hero.37958/
-http://schlangan.free.fr/mvxgen.html
-https://sanderfrenken.github.io/Universal-LPC-Spritesheet-Character-Generator/
-http://gaurav.munjal.us/Universal-LPC-Spritesheet-Character-Generator/
-http://lpc.opengameart.org/static/lpc-style-guide/assets.html
-https://chibi.center/
+# Game dev in elm
--- src/Main.elm
+++ src/Main.elm
@@ -17,6 +17,15 @@ import Task
 import WebGL
 
 
+
+--https://forums.rpgmakerweb.com/index.php?threads/mv-added-resource-generators-a-face-for-your-hero.37958/
+--http://schlangan.free.fr/mvxgen.html
+--https://sanderfrenken.github.io/Universal-LPC-Spritesheet-Character-Generator/
+--http://gaurav.munjal.us/Universal-LPC-Spritesheet-Character-Generator/
+--http://lpc.opengameart.org/static/lpc-style-guide/assets.html
+--https://chibi.center/
+
+
 initModel : Game.Model
 initModel =
     { textures = { done = Dict.empty, loading = Set.empty }
